b871fad3feecbe0eef423864b8c46ffddeb4ebbe
Sasha Chepurniy <sloth04ua@gmail.com>
2021-11-08 13:38:00 +0300

    Cleared, Task solved with .split

--- main.py
+++ main.py
@@ -12,23 +12,6 @@ look_for_code = {'62W4960182644872': 'Канівська ГЕС', '62W8027183829
 look_for_type = 'A01'
 
 
-def find_and_save(df, target_dic, target_type):
-    step = 0
-    df_re = pd.DataFrame()
-    df_re.index = df.index
-    for item in range(df.shape[1]):
-        temp_list = df.columns[item].split(' ')
-        if target_type in temp_list:
-            df_re = pd.concat([df_re, df[df.columns[item]]], axis=1)
-            df_re.rename(columns={df_re.columns[step]: temp_list[2] + ' ' + temp_list[3]}, inplace=True)
-            step += 1
-    df.fillna(0, inplace=True)
-    df.replace(np.nan, 0, regex=True, inplace=True)
-    print(df_re)
-    # print(step)
-    return df_re
-
-
 def create_df(name):
     df = pd.read_csv(name, sep=';', index_col=0)
     df.index.name = 'Date'
@@ -55,7 +38,6 @@ def create_df(name):
         if float(col_list[-1]) > float(ver.get(wc)):
             ver.update({wc: col_list[-1]})
             ver_i.update({wc: item})
-        # print(df_re)
     added = 0
     for i in ver_i.keys():
         df_re = pd.concat([df_re, df[df.columns[ver_i.get(i)-1]]], axis=1)
@@ -63,8 +45,6 @@ def create_df(name):
         added += 1
     df_re.fillna(0, inplace=True)
     df_re.replace(np.nan, 0, regex=True, inplace=True)
-    # print(ver)
-    # print(ver_i)
     return df_re
 
 
@@ -78,11 +58,7 @@ def main():
         name_df = create_df(item)
         list_csv.append(name_df)
     united_df = pd.concat(list_csv)
-    # print(united_df)
-    # print(united_df)
-    # selected_df = find_and_save(united_df, look_for_code, look_for_type)
     united_df.to_csv('output_result.csv', sep=';')
-    # selected_df.to_excel('output_result.xlsx')
 
 
 if __name__ == '__main__':
