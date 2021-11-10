22e906b70788bc261e8d86548b6d8e5f0f17027b
ortecs <!76Z_SP9s*sX#QW>
2021-11-10 17:19:30 +0200

    improve

--- main.py
+++ main.py
@@ -13,36 +13,25 @@ look_for_type = 'A01'
 
 
 def create_df(name):
+    df_re = pd.DataFrame()
     df = pd.read_csv(name, sep=';', index_col=0)
-    df.index.name = 'Date'
     df.sort_index(axis=1)
     df.index = df.index.map(lambda x: str(x)[:-8])
     df.index = pd.to_datetime(df.index)
     df = df.groupby(pd.Grouper(freq='H')).mean()
-    columns = df.columns
-    df_re = pd.DataFrame()
-    ver = dict.fromkeys(look_for_code.keys())
-    ver_i = dict.fromkeys(look_for_code.keys())
-    for item in ver.keys():
-        ver[item] = 0
-        ver_i[item] = 0
-    item = 0
-    for col in columns:
-        item += 1
-        col_list = col.split()
+    ver = {k: {'index': 0, 'version': 0} for k in look_for_code}
+    for i, column in enumerate(df.columns):
+        col_list = column.split()
         if look_for_type not in col_list:
             continue
         elif not col_list[col_list.index(look_for_type) - 1] in look_for_code.keys():
             continue
         wc = col_list[col_list.index(look_for_type) - 1]
-        if float(col_list[-1]) > float(ver.get(wc)):
-            ver.update({wc: col_list[-1]})
-            ver_i.update({wc: item})
-    added = 0
-    for i in ver_i.keys():
-        df_re = pd.concat([df_re, df[df.columns[ver_i.get(i)-1]]], axis=1)
-        df_re.rename(columns={df_re.columns[added]: i + ' ' + look_for_code[i]}, inplace=True)
-        added += 1
+        if float(col_list[-1]) > float(ver.get(wc).get('version')):
+            ver.update({wc: {'index': i, 'version': col_list[-1]}})
+    for i, w in enumerate(look_for_code.keys()):
+        df_re = pd.concat([df_re, df[df.columns[ver.get(w).get('index')]]], axis=1)
+        df_re.rename(columns={df_re.columns[i]: f'{w} {look_for_code[w]}'}, inplace=True)
     df_re.fillna(0, inplace=True)
     df_re.replace(np.nan, 0, regex=True, inplace=True)
     return df_re
